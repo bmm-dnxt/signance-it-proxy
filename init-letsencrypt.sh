94103c7f2113f1646fded56118d6a7d1b457dd07
Pedro Santos <aps@digitalnext.pt>
2021-11-19 18:11:31 +0000

    add .dxnt.pt domains to certificate request

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(rsa.dnxt.space jitsi.dnxt.space mattermost.dnxt.space owncloud.dnxt.space etherpad.dnxt.space)
+domains=(rsa.dnxt.space jitsi.dnxt.space mattermost.dnxt.space owncloud.dnxt.space etherpad.dnxt.space jitsi.dnxt.pt mattermost.dnxt.pt etherpad.dnxt.pt owncloud.dnxt.pt)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="aps@digitalnext.pt" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
